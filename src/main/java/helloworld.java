0db0cc0bcf85d9cea2630807b77bfebeeedbae5e
vanipatoju <vanipatoju@gmail.com>
2017-04-24 17:18:19 +0530

    commit changes

---
+++ src/main/java/helloworld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Anil on 1/5/17.
+ */
+import org.apache.log4j.Logger;
+
+public class helloworld {
+  Logger logger;
+  public static void main(String[] args){
+
+    System.out.println("Hello wolrd");
+  }
+}
---
+++ src/test/java/TestSample.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Anil on 1/5/17.
+ */
+public class TestSample extends Assert {
+
+  @Test
+  public void firstTest(){
+   Assert.assertTrue("test passed", 2>1);
+  }
+}
